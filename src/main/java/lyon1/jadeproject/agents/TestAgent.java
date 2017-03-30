286b05618dec6a764409b929c611797e166afcbb
Dyavil <dyavil.q@gmail.com>
2017-03-30 11:03:25 +0200

    start project

---
+++ src/main/java/lyon1/jadeproject/agents/TestAgent.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lyon1.jadeproject.agents;
+
+import jade.core.Agent;
+
+/**
+ *
+ * @author dyavil
+ */
+public class TestAgent extends Agent {
+    
+}
